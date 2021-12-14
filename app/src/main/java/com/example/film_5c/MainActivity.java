8394e5062944debd79001d41951da0dfaac6a969
SimonVentura <vsimone474@gmail.com>
2021-12-14 11:43:09 +0100

    Commit fine ora

--- app/src/main/java/com/example/film_5c/MainActivity.java
+++ app/src/main/java/com/example/film_5c/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.film_5c;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String film[]= {"Dune","Spiderman","Titanic","Batman"};
+    ListView lvFilm;
+    Button btnEsci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvFilm= (ListView)findViewById(R.id.lvFilm);
+
+        ArrayAdapter<String> aaFilm= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, film);
+        lvFilm.setAdapter(aaFilm);
+
+        lvFilm.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String titolo= (String)lvFilm.getItemAtPosition(i);
+                Toast.makeText(getApplicationContext(), titolo, Toast.LENGTH_LONG).show();
+                Log.d("click_procedura", "Hai cliccato sulla " + i + " voce");
+            }
+        });
+
+        btnEsci= (Button)findViewById(R.id.btnEsci);
+
+        btnEsci.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "stai uscendo", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
